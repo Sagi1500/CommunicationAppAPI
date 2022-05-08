498dc6e5b9a4321855f6f32c38b7772fb56951b3
Sagi1500 <Sagi1500@gmail.com>
2022-05-08 20:54:02 +0300

    Add project files.

---
+++ RatingCMV/Models/Rating.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommunicationAppAPI.Models
+{
+    public class Rating
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z0-9]+$")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Range(1,5)]
+        public int Rate { get; set; }
+
+        [StringLength(2000)]
+        public string? Description { get; set; }
+
+        public DateTime Time { get; set; }
+
+    }
+}
---
+++ RatingCMV/RatingsContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CommunicationAppAPI.Models;
+
+namespace CommunicationAppAPI
+{
+    public class RatingsContext : DbContext
+    {
+        private const string connectionString = "server=localhost;port=3306;database=Ratings;user=root;password=SagiShoval";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString, MariaDbServerVersion.AutoDetect(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuring the Name property as the primary
+            // key of the Items table
+            modelBuilder.Entity<Rating>().HasKey(e => e.Id);
+        }
+
+        public DbSet<Rating>? Ratings { get; set; }
+    }
+}
