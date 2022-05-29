c55f6d8d32c46567bfaf1d4996c2896a60c72678
Sagi1500 <Sagi1500@gmail.com>
2022-05-29 21:43:30 +0300

    add button

--- RatingCMV/Program.cs
+++ RatingCMV/Program.cs
@@ -54,6 +54,6 @@ app.UseStaticFiles();
 app.UseRouting();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Ratings}/{action=Ajax}/{id?}");
+    pattern: "{controller=Ratings}/{action=Index}/{id?}");
 
 app.Run();
