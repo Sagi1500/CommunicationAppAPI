29e7a60b74fb33b790066b6cafaec9cea9ed49f4
Sagi1500 <Sagi1500@gmail.com>
2022-06-17 17:38:58 +0300

    update transfer to firebase (working only with manualy adding token)

--- CommunicationApi/Controllers/TransferController.cs
+++ CommunicationApi/Controllers/TransferController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Domain;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
 
 namespace CommunicationApi.Controllers
 {
@@ -13,13 +16,34 @@ namespace CommunicationApi.Controllers
         private readonly MessagesServices _messagesService;
         private readonly UsersServices _userService;
         private readonly ContactsServices _contactsService;
+        private static Dictionary<string, string> _firebaseTokens;
 
         // Constructor.
-        public TransferController(MessagesServices messagesService, UsersServices usersServices,ContactsServices contactsServices)
+        public TransferController(MessagesServices messagesService, UsersServices usersServices, ContactsServices contactsServices)
         {
             _messagesService = messagesService;
             _userService = usersServices;
             _contactsService = contactsServices;
+            initializeDict();
+        }
+
+
+        static void initializeDict()
+        {
+            if (_firebaseTokens == null)
+            {
+                _firebaseTokens = new Dictionary<string, string>(); 
+
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromFile("private_key.json")
+                });
+
+                if (!_firebaseTokens.ContainsKey("a1"))
+                {
+                    _firebaseTokens.Add("a1", "f8Y5bZ2APZI:APA91bFT1eLsrRXWmRUAdbrgLdwTxY2QW7NXEoOu7jLHyUyk9J7XuIaifX5tm-J4JrcbPCD4-5XXFX86JSAgTD-MudyI3hjWK6pmke1kmS9EB7jBDHQnD26uwn0BFGPZXV6604whrzau");
+                }
+            }
         }
 
 
@@ -36,7 +60,7 @@ namespace CommunicationApi.Controllers
             }
 
             // create contact.
-            Message message = InitializeMessage(transfer.From, transfer.To, transfer.Content);
+            Domain.Message message = InitializeMessage(transfer.From, transfer.To, transfer.Content);
 
             // Adding the contact to the logged in server.
             var res = await _messagesService.CreateNewMessage(message);
@@ -52,13 +76,35 @@ namespace CommunicationApi.Controllers
                 return BadRequest();
             }
             
+            if (_firebaseTokens.ContainsKey(transfer.To))
+            {
+              
+                var registrationTokens = _firebaseTokens[transfer.To];
+
+                var fbMessage = new FirebaseAdmin.Messaging.Message()
+                {
+                    Data = new Dictionary<string, string>()
+                    {
+                        { "myData", "1337" }
+                    },
+                    Token = registrationTokens,
+                    Notification = new Notification()
+                    {
+                        Title = "Message from " + transfer.From,
+                        Body = transfer.Content,
+                    }
+                };
+
+                string Response = await FirebaseMessaging.DefaultInstance.SendAsync(fbMessage);
+            }
+
             return Created("/api/Contacts/"+ transfer.From + "/Messages/",message);
         }
 
         // The function recieves the invitation information and the logged in user and create contact.
-        private Message InitializeMessage(string ContactId, string UserId, string Content)
+        private Domain.Message InitializeMessage(string ContactId, string UserId, string Content)
         {
-            Message newMessage = new Message();
+            Domain.Message newMessage = new Domain.Message();
             newMessage.ContactId = ContactId;
             newMessage.UserId = UserId;
             newMessage.Content = Content;
